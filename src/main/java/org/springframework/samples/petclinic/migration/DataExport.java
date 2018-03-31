d6a8481ad8b3888779072f7785c2a60778332f90
marcdragon123 <thedragon_master@hotmail.ca>
2018-03-31 16:30:28 -0400

    Migration forklift part 1

--- src/main/java/org/springframework/samples/petclinic/migration/DataExport.java
+++ src/main/java/org/springframework/samples/petclinic/migration/DataExport.java
@@ -1,10 +1,50 @@
 package org.springframework.samples.petclinic.migration;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DataExport {
 
+
+    private ArrayList<HashMap<String, String>> set = new ArrayList<>();
+
     public void getData() {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/petclinic", "root", "petclinic");
+
+            Statement stmt = con.createStatement();
+
+            stmt.executeQuery("USE petclinic");
+
+
+            ResultSet res = stmt.executeQuery("SELECT * FROM owners;");
+
+            ResultSetMetaData md = res.getMetaData();
+
+            while (res.next()) {
+               HashMap<String, String> results = new HashMap<>();
+               System.out.println(res.getInt(1)+"  "+res.getString(2)+"  "+res.getString(3));
+
+
+               for(int i =0; i<md.getColumnCount(); i++) {
+                   if (md.getColumnName(i)!=null && res.getString(i)!=null) {
+                       results.put(md.getColumnName(i), res.getString(i));
+                   }
+               }
+
+               set.add(results);
+           }
+
+           con.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
 
     }
+
 
 
 }
--- src/main/java/org/springframework/samples/petclinic/migration/JSONConverter.java
+++ src/main/java/org/springframework/samples/petclinic/migration/JSONConverter.java
@@ -1,5 +1,28 @@
 package org.springframework.samples.petclinic.migration;
 
+import java.io.IOException;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.*;
+import java.util.Set;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+
 public class JSONConverter {
+
+    public void convert(ArrayList<HashMap> list) throws IOException {
+        JSONObject obj = new JSONObject();
+        JSONArray company = new JSONArray();
+
+        for (HashMap map: list) {
+
+            /*for (Set<Entry> e : map.entrySet()) {
+                return;
+            }*/
+        }
+    }
 
 }
